543973717eaedfc12f0536301f3b271543f63735
Curtis H <sitrucrettah1@gmail.com>
2020-12-17 19:52:39 -0800

    Add more translators with buttons: pirate/groot function without limitations.

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ $("#pirate-translate").on("click", function () {
         $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
     })
 })
+
+$("#yoda-translate").on("click", function () {
+    var text = "Hello sir, my mother goes with me to the ocean.";
+    var queryURL = "https://api.funtranslations.com/translate/yoda.json?text=" + text;
+    $.ajax({
+        // headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
+    })
+})
+
+$("#hodor-translate").on("click", function () {
+    var text = "Hello sir, my mother goes with me to the ocean.";
+    var queryURL = "https://api.funtranslations.com/translate/hodor.json?text=" + text;
+    $.ajax({
+        headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
+    })
+})
+
+$("#groot-translate").on("click", function () {
+    var text = "Hello sir, my mother goes with me to the ocean.";
+    var queryURL = "https://api.funtranslations.com/translate/groot.json?text=" + text;
+    $.ajax({
+        // headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
+    })
+})
