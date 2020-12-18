cc28ca9ac6aab52466cd9a2b5acfcb0c90d0f553
Curtis H <sitrucrettah1@gmail.com>
2020-12-17 18:44:23 -0800

    Translation works

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var text = "Hello sir.";
-
+var text = "Hello sir, my mother goes with me to the ocean.";
+text = "Breathe out, So I can breathe you in, Hold you in, And now, I know you've always been, Out of your head, Out of my head, I sang";
 var queryURL = "https://api.funtranslations.com/translate/pirate.json?text=" + text;
 
 $.ajax({
+    headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
     url: queryURL,
-    method: "GET",
-    header: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' }
+    method: "GET"
 }).then(function (response) {
     console.log(response);
     var texting = $("<div>");
