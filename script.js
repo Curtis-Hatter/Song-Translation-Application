274a19a2966bdb4baf6bb171da3c21133ed8aa8f
Curtis H <sitrucrettah1@gmail.com>
2020-12-17 19:22:30 -0800

    Ajax works, Click function works

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// var text = "Hello sir, my mother goes with me to the ocean.";
-// text = "Breathe out, So I can breathe you in, Hold you in, And now, I know you've always been, Out of your head, Out of my head, I sang";
-// var queryURL = "https://api.funtranslations.com/translate/pirate.json?text=" + text;
+// queryURL = "https://api.funtranslations.com/translate/yoda.json?text=" + text;
 
-// $.ajax({
-//     headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-//     var texting = $("<div>");
-//     texting.text(JSON.stringify(response.contents.translated));
-//     $(".text-here").append(texting);
-// })
+$("#pirate-translate").on("click", function () {
+    var text = "Hello sir, my mother goes with me to the ocean.";
+    var queryURL = "https://api.funtranslations.com/translate/pirate.json?text=" + text;
+    $.ajax({
+        headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
+    })
+})
