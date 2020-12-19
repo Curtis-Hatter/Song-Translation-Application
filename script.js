51aea25ddb01165a58537ac549cff0bf5d3fa221
Megan Bryan <meganmbryan12@gmail.com>
2020-12-19 12:48:58 -0800

    Add loading symbols to buttons

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $("#song-button").on('click', function () {
     var url = "https://api.musixmatch.com/ws/1.1/track.search?q_track=" + userinput + "&page_size=1&page=1&s_track_rating=desc&apikey=e6524e95459dac73ce4a95bde9428b70";
     var corsfix = "https://cors-anywhere.herokuapp.com/";
     var fullurl = corsfix + url;
+    $("#song-button").addClass("is-loading")
 
     console.log(userinput);
     $.ajax({
@@ -14,7 +15,7 @@ $("#song-button").on('click', function () {
         console.log(idobject);
         var trackcheck = idobject.message.body.track_list[0].track;
         var trackid = idobject.message.body.track_list[0].track.track_id;
-
+        $("#song-button").removeClass("is-loading")
 
         console.log(trackid);
 
@@ -50,12 +51,14 @@ $("#pirate-translate").on("click", function () {
     // var text = "Hello sir, my mother goes with me to the ocean.";
     var queryURL = "https://api.funtranslations.com/translate/pirate.json?text=" + text;
     console.log(queryURL);
+    $("#pirate-translate").addClass("is-loading")
     $.ajax({
         headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // console.log(response);
+        $("#pirate-translate").removeClass("is-loading")
         $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
     })
 })
@@ -63,12 +66,14 @@ $("#pirate-translate").on("click", function () {
 $("#yoda-translate").on("click", function () {
     // var text = "Hello sir, my mother goes with me to the ocean.";
     var queryURL = "https://api.funtranslations.com/translate/yoda.json?text=" + text;
+    $("#yoda-translate").addClass("is-loading")
     $.ajax({
         // headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // console.log(response);
+        $("#yoda-translate").removeClass("is-loading")
         $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
     })
 })
@@ -76,12 +81,14 @@ $("#yoda-translate").on("click", function () {
 $("#hodor-translate").on("click", function () {
     // var text = "Hello sir, my mother goes with me to the ocean.";
     var queryURL = "https://api.funtranslations.com/translate/hodor.json?text=" + text;
+    $("#hodor-translate").addClass("is-loading")
     $.ajax({
         // headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // console.log(response);
+        $("#hodor-translate").removeClass("is-loading")
         $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
     })
 })
@@ -89,12 +96,14 @@ $("#hodor-translate").on("click", function () {
 $("#groot-translate").on("click", function () {
     // var text = "Hello sir, my mother goes with me to the ocean.";
     var queryURL = "https://api.funtranslations.com/translate/groot.json?text=" + text;
+    $("#groot-translate").addClass("is-loading")
     $.ajax({
         // headers: { 'X-FunTranslations-Api-Secret': 'Ta_kah9NbJ1OJsOMUdhyBQeF' },
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // console.log(response);
+        $("#groot-translate").removeClass("is-loading")
         $("#retrieved-translation").text(JSON.stringify(response.contents.translated));
     })
 })
